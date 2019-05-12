b55198b91431977812286ed807c6d3339c485f47
jxz158 <jzxiaochengxu001@outlook.com>
2019-05-11 21:26:58 -0400

    Calling the weather API and update view dynamically

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,32 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
 Page({
-  onLoad(){
-    console.log("Hello World!");
+  data: {
+    nowTemp: '',
+    nowWeather: ''
+  },
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '广州市'
+      },
+      success: res => { // The statement success(res){} does not work. The program complains about null error of the variable this
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather]
+        })
+      }
+    })
   }
 })
